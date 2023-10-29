3e1bfd05c9a5ab9a11d1600a62a55d5e78a0fb63
fb0801 <fbhatti0801@gmail.com>
2023-10-29 18:00:46 +0000

    speech recg transcript 2

--- speech-recg/main.py
+++ speech-recg/main.py
@@ -6,27 +6,41 @@ import sys
 upload_endpoint = "https://api.assemblyai.com/v2/upload"
 transcript_endpoint = "https://api.assemblyai.com/v2/transcript"
 
+
+headers = {'authorization': API_KEY_ASSEMBLYAI}
+
+
 #upload
 filename = sys.argv[1]
 
-def read_file(filename, chunk_size = 5242880):
-    with open(filename, 'rb') as _file:
-        while True:
-            data = _file.read(chunk_size)
-            if not data:
-                break
-            yield data
-headers = {'authorization': API_KEY_ASSEMBLYAI}
-response = requests.post(upload_endpoint,
-                         headers=headers,
-                         data= read_file(filename))
+def upload(filename):
+    def read_file(filename, chunk_size = 5242880):
+        with open(filename, 'rb') as _file:
+            while True:
+                data = _file.read(chunk_size)
+                if not data:
+                    break
+                yield data
+    upload_response = requests.post(upload_endpoint,
+                            headers=headers,
+                            data= read_file(filename))
 
-print(response.json())
 
-audio_url = response.json()['upload_url']
+    audio_url = upload_response.json()['upload_url']
+    return audio_url
 
-#transcript
-json = {"audio_url": audio_url}
-response = requests.post(transcript_endpoint, json= json, headers=headers)
-print(response.json())
+#transcribe
+def transcribe(audio_url):
+    transcript_request = {"audio_url": audio_url}
+    transcript_response = requests.post(transcript_endpoint, json= transcript_request, headers=headers)
+    job_id = transcript_response.json()['id']
+    return job_id
 
+audio_url = upload(filename)
+job_id = transcribe(audio_url)
+
+print(job_id)
+#poll
+
+
+#save transcript
