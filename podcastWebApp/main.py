9ea0e88cd2044f24f7a62dde576b0a3de5c72a12
fb0801 <fbhatti0801@gmail.com>
2024-01-30 16:00:10 +0000

    setup 4

--- podcastWebApp/main.py
+++ podcastWebApp/main.py
@@ -6,7 +6,16 @@ st.title('podcast summeries')
 episode_id = st.sidebar.text_input('input eposode id')
 button = st.sidebar.button("Download Episode summary", on_click=save_transcript, args=(episode_id,))
 
-
+def get_clean_time(start_ms):
+    seconds = int((start_ms / 1000) % 60)
+    minutes = int((start_ms / (1000 * 60)) % 60)
+    hours = int((start_ms / (1000 * 60 * 60)) % 24)
+    if hours > 0:
+        start_t = f'{hours:02d}:{minutes:02d}:{seconds:02d}'
+    else:
+        start_t = f'{minutes:02d}:{seconds:02d}'
+        
+    return start_t
 
 if button:
     filename = episode_id + '_chapters.json'
